065a97b514ad707a5083db8cab8b4ffdad09f5bf
Gerald Png <gpng@estl.sg>
2020-12-05 12:18:38 +0800

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import React, { FC } from 'react';
+
+const Index: FC = () => {
+  return (
+    <div className="root">
+      <h1>Hello World</h1>
+      <style jsx>
+        {`
+          .root {
+          }
+        `}
+      </style>
+    </div>
+  );
+};
+
+export default Index;
